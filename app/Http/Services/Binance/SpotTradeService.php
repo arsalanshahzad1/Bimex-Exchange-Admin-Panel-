9a6647ab85e92d6ae861b4824317c8031603a4af
fg72072 <fg72072@gmail.com>
2023-03-03 21:49:38 +0500

    my trade,all orders, open orders

--- app/Http/Services/Binance/SpotTradeService.php
+++ app/Http/Services/Binance/SpotTradeService.php
@@ -33,7 +33,6 @@ class SpotTradeService
                 'message' => $e->getMessage()
             ];
         }
-
     }
 
     public function getOrderBook($params = [])
@@ -55,29 +54,145 @@ class SpotTradeService
                 'message' => $e->getMessage()
             ];
         }
-
+    }
+    // get 24 ticker
+    public function get24Ticker($params = [])
+    {
+        try {
+            $url = $this->BASE_URL . "api/v3/ticker/24hr?";
+            $query = http_build_query($params);
+            $response = Http::get($url . $query);
+            $data = $response->json();
+            $data = array_slice($data, 0, 10);
+            return [
+                'data' => $data,
+                'success' => true,
+                'message' => 'Success'
+            ];
+        } catch (\Exception $e) {
+            return [
+                'data' => [],
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
     }
 
     public function createOrder($params = [], $keys = [])
     {
         try {
-            $url = $this->BASE_URL . "api/v3/order/test?";
+            $url = $this->BASE_URL . "api/v3/order?";
             $hash = signature($params, $keys['secret']);
             $query = $hash['query'];
             $sign = $hash['sign'];
-
-            $response = Http::withHeaders(['X-MBX-APIKEY' => $keys['api']])
-                ->post($url . $query . '&signature=' . $sign, []);
-
+            $response = Http::withHeaders(['X-MBX-APIKEY' => $keys['api']])->post($url . $query . '&signature=' . $sign);
             $data = $response->json();
-
-
             if (isset($data["code"])) {
-                return \Response::json($response->json(), 400);
+                return [
+                    'data' => $data,
+                    'success' => false,
+                    'message' => $data['msg']
+                ];
             }
+            return [
+                'data' => $data,
+                'success' => true,
+                'message' => 'Success.'
+            ];
+        } catch (\Exception $e) {
+            return [
+                'data' => [],
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+    // get open orders
+    public function getOpenOrders($params = [], $keys = [])
+    {
+        try {
+            $url = $this->BASE_URL . "api/v3/order?";
+            $hash = signature($params, $keys['secret']);
+            $query = $hash['query'];
+            $sign = $hash['sign'];
+            $response = Http::withHeaders(['X-MBX-APIKEY' => $keys['api']])
+                ->get($url . $query . '&signature=' . $sign);
+            $data = $response->json();
+            if (isset($data["code"])) {
+                return [
+                    'data' => [],
+                    'success' => false,
+                    'message' => $data['msg']
+                ];
+            }
+            return [
+                'data' => $data,
+                'success' => true,
+                'message' => 'Success.'
+            ];
+        } catch (\Exception $e) {
+            return [
+                'data' => [],
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+    // get all orders
+    public function getAllOrders($params = [], $keys = [])
+    {
+        try {
+            $url = $this->BASE_URL . "api/v3/allOrders?";
+            $hash = signature($params, $keys['secret']);
+            $query = $hash['query'];
+            $sign = $hash['sign'];
+            $response = Http::withHeaders(['X-MBX-APIKEY' => $keys['api']])
+                ->get($url . $query . '&signature=' . $sign);
+            $data = $response->json();
+            if (isset($data["code"])) {
+                return [
+                    'data' => [],
+                    'success' => false,
+                    'message' => $data['msg']
+                ];
+            }
+            return [
+                'data' => $data,
+                'success' => true,
+                'message' => 'Success.'
+            ];
+        } catch (\Exception $e) {
+            return [
+                'data' => [],
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
 
-            return $data;
-
+    // get my trade history
+    public function getMyTradeHistory($params = [], $keys = [])
+    {
+        try {
+            $url = $this->BASE_URL . "api/v3/myTrades?";
+            $hash = signature($params, $keys['secret']);
+            $query = $hash['query'];
+            $sign = $hash['sign'];
+            $response = Http::withHeaders(['X-MBX-APIKEY' => $keys['api']])
+                ->get($url . $query . '&signature=' . $sign);
+            $data = $response->json();
+            if (isset($data["code"])) {
+                return [
+                    'data' => [],
+                    'success' => false,
+                    'message' => $data['msg']
+                ];
+            }
+            return [
+                'data' => $data,
+                'success' => true,
+                'message' => 'Success.'
+            ];
         } catch (\Exception $e) {
             return [
                 'data' => [],
